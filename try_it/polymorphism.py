7785280c697964999cf962176a95195b9dbc414d
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-29 22:42:02 +0530

    Create polymorphism.py

---
+++ try_it/polymorphism.py
@@ -0,0 +1,36 @@
+from abc import ABC , abstractmethod
+
+class Car(ABC):
+	@abstractmethod
+	def sound(self):
+		pass
+
+
+class Honda(Car):
+	def sound(self):
+		return "phooo!"
+
+class Benz(Car):
+	def sound(self):
+		return "phaaa!"
+
+class Vw(Car):
+	def sound(self):
+		return "phee!"
+
+# def horn(car):
+# 	print(car.sound())
+
+Accord = Honda()
+S_class = Benz()
+gti = Vw()
+
+print(Accord.sound())
+print(S_class.sound())
+print(gti.sound())
+# horn(Accord)
+# horn(S_class)
+# horn(Vw)
+#
+
+
