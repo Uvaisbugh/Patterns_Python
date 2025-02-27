918951d1f6dff799c315bc9b8be6362bffb687d4
Uvais Bughari <suvaisbughari@gmail.com>
2025-02-27 18:56:43 +0530

    Update q23_fibnacci_lambda.py

--- try_it/q23_fibnacci_lambda.py
+++ try_it/q23_fibnacci_lambda.py
@@ -4,3 +4,10 @@ febnacci_series = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2
 
 n = 10
 print(febnacci_series(n))
+
+# def febnacci(n):
+#     if n <= 1:
+#         return n
+#     return febnacci(n-1) + febnacci(n-2)
+# print(febnacci(9))
+
