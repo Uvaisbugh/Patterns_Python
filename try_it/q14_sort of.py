64fd8246a82a729a979e0088c6ad4e1a48366df6
Uvais Bughari <suvaisbughari@gmail.com>
2025-02-27 18:56:48 +0530

    Update q14_sort of.py

--- try_it/q14_sort of.py
+++ try_it/q14_sort of.py
@@ -2,11 +2,10 @@ def sum_of_differences(num):
 	num.sort(reverse=True)
 
 	diff_sum = 0
-	for i in range(1,len(num)):
-		diff_sum +=num[i-1]-num[i]
+	for i in range(1, len(num)):
+		diff_sum += num[i - 1] - num[i]
 
 	return diff_sum
-
 
 
 numbers = [10, 20, 5, 40, 15]
