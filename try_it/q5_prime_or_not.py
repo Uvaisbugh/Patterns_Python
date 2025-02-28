4bcddbe8f86325966a85c8968de92e53ff9d0bf8
Uvais Bughari <suvaisbughari@gmail.com>
2025-02-28 20:20:21 +0530

    Update q5_prime_or_not.py

--- try_it/q5_prime_or_not.py
+++ try_it/q5_prime_or_not.py
@@ -1,10 +1,11 @@
 def is_prime(n):
 	if n < 2:
 		return False
-	for i in range(2, int(n** 0.5) +1):
-		if n % i ==0 :
+	for i in range(2, int(n ** 0.5) + 1):
+		if n % i == 0:
 			return False
-	return  True
+	return True
 
-num =28
+
+num = 28
 print(is_prime(num))
